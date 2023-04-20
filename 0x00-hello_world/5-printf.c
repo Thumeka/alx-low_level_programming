08868d5788dc78d34fcc3d0e78987184a6e9c1ec
Thumeka <thumekatembani21@gmail.com>
2023-04-20 15:18:03 -0700

    Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
+
 /**
- * main -  program that prints using printf
- * Return: 0 (Success)
+ * main - This prints with proper grammar, but the outcome is a piece of art
+ * Return: This returns 0
  */
+
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art\n");
-Return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+Return(0);
 }
