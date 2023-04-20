c771779966754c4104ff0b8f1a749beb7db19b57
Thumeka <thumekatembani21@gmail.com>
2023-04-20 14:39:23 -0700

    Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,9 +2,9 @@
 
 /**
  * main -  program that prints using printf
- *
- * Return: Always 0
+ * Return: 0 (Success)
  */
+
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art\n");
