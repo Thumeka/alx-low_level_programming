df41d91093292f3fc8f3adf094c644d03af8d09f
Thumeka <thumekatembani21@gmail.com>
2023-04-25 09:38:17 -0700

    Task 3

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -11,7 +11,7 @@ void print_alphabet_x10(void)
 	int count = 0;
 	char alphabet;
 
-	while (count++ <= 9)
+	while (count++ <= `9)
 	{
 		for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 			_putchar(alphabet);
---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _islower - Write a function that checks for lowercase character.
+ *@c: char
+ * Return: 1 if char is lowercase, otherwise 0.
+ */
+int _islower(int c)
+{
+	char c;
+
+	if (c >= 'a' && c <= 'z';
+		return (1);
+	else
+		return (0);
+}
