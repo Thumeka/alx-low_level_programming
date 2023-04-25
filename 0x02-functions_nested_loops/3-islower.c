a268a2a926fcd4f20816fcc71078e04daa3e3747
Thumeka <thumekatembani21@gmail.com>
2023-04-25 10:07:00 -0700

    task 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -5,9 +5,11 @@
  *@c: char
  * Return: 1 if char is lowercase, otherwise 0.
  */
+
 int _islower(int c)
 {
-	if (c >= 'a' && c <= 'z');
+
+	if (c >= 'a' && c <= 'z')
 		return (1);
 	else
 		return (0);
