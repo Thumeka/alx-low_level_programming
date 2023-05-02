f65a30a11e0403ea730729dfee21da0547ca0544
Thumeka <thumekatembani21@gmail.com>
2023-05-02 11:11:55 -0700

    T3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _puts - function that prints a string,
+ * followed by a new line, to stdout.
+ * @str: char
+ *
+ * Return: sring length
+ */
+
+void _puts(char *str)
+{
+	int len = 0;
+
+	while (str[len] != '\0')
+	{
+		_putchar(str[len]);
+		len++;
+	}
+	_putchar('\n');
+}
