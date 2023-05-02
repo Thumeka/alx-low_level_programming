d7d2dd815f96063823b319959b890c1af23cff3e
Thumeka <thumekatembani21@gmail.com>
2023-05-02 16:21:09 -0700

    T10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -9,11 +9,33 @@
 
 int _atoi(char *s)
 {
-	int num = 0;
-	int i;
+	int j = 0;
+	unsigned int ni = 0;
+	int max = 1;
+	int min = 0;
 
-	for (i = 0; s[i] != 0; i++)
-		num = num * 10 + s[i] - '0';
+	while (s[j])
+	{
+		if (s[j] == 45)
+		{
+			max *= -1;
+		}
 
-	return (num);
+		while (s[j] >= 48 && s[j] <= 57)
+		{
+			min = 1;
+			ni = (ni * 10) + (s[j] - '0');
+			j++;
+		}
+
+		if (min == 1)
+		{
+			break;
+		}
+
+		j++;
+	}
+
+	ni *= max;
+	return (ni);
 }
