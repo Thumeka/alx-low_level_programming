7d0929a9da5ea43b61d5fdc37e0508d287966223
Thumeka <thumekatembani21@gmail.com>
2023-05-05 11:46:08 -0700

    T9

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -9,6 +9,8 @@
 
 void print_number(int n)
 {
+	unsigned int b;
+
 	if (n < 0)
 	{
 		_putchar('-');
@@ -19,8 +21,8 @@ void print_number(int n)
 	if (n == 0)
 		_putchar('0');
 
-	if (n / 10)
-		print_number(n / 10);
+	if (b / 10)
+		print_number(b / 10);
 
-	_putchar(n % 10 + '0');
+	_putchar(b % 10 + '0');
 }
