969739c2c5ceb14ed4aea77e12eec4a6819dd781
Thumeka <thumekatembani21@gmail.com>
2023-05-08 07:47:41 -0700

    T4

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strpbrk - searches a string for any set of bytes.
+ * @s: string
+ * @accept: bytes
+ *
+ * Return: s that matches bytes in accept or NULL
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	char *k;
+
+	while (*s != '\0')
+	{
+		k = accept;
+
+		while (*k != '\0')
+		{
+			if (*s == *k)
+			{
+				return (s);
+			}
+			k++;
+		}
+		s++;
+	}
+
+	return (NULL);
+}
