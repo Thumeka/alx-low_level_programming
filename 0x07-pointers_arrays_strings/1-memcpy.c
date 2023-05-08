65bfbab271bc956548b089fb17a1624e361ca19e
Thumeka <thumekatembani21@gmail.com>
2023-05-08 06:06:30 -0700

    T1

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _memcpy - function that copies memory area.
+ * @n: bytes
+ * @src: from  memory area
+ * @dest: to  memory area
+ *
+ * Return: dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int k, f;
+
+	while (n--)
+	{
+		dest[k++] = src[f++];
+	}
+
+	return (dest);
+}
