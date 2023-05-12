03b9d3d10da3506e79190bb9b72a93ba7530b431
Thumeka <thumekatembani21@gmail.com>
2023-05-12 02:43:00 -0700

    T0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints its name with a new line.
+ * @argc: count
+ * @argv: vector
+ *
+ * Return: 0.
+ */
+int main(int argc, char *argv)
+{
+	printf("%d\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+int _putchar(char c);
+
+#include <stdlib.h>
+#include <time.h>
+
+#endif /* MAIN_H */
