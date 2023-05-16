ad2dccda6fe45107c2e7b58ba210e2fdc621312a
Thumeka <thumekatembani21@gmail.com>
2023-05-16 03:46:29 -0700

    T2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -2,50 +2,53 @@
 #include <stdlib.h>
 
 /**
- * str_concat -  concatenates two strings.
+ * str_concat - concatenates two strings.
  * @s1: first
  * @s2: second
  *
- * Return: pointer to new memory or NULL
+ * Return: NULL, pointer to new allocated memory.
  */
 
 char *str_concat(char *s1, char *s2)
 {
 	char *arr;
-	int a, b, f, g;
+	int f = 0, g = 0, l1 = 0, l2 = 0;
 
 	if (s1 == NULL)
 	{
-		s1 = " ";
+		s1 = "";
 	}
 
 	if (s2 == NULL)
 	{
-		s2 = " ";
+		s2 = "";
 	}
 
-	for (b = 0; s1[b] != '\0'; b++)
-		;
+	while (s1[l1] != '\0')
+	{
+		l1++;
+	}
 
-	for (a = 0; s2[a] != '\0'; a++)
-		;
+	while (s2[l2] != '\0')
+	{
+		l2++;
+	}
 
-	arr = malloc(sizeof(char) * (a + b + 1));
-
+	arr = malloc((l1 + l2 + 1) * sizeof(char));
 	if (arr == NULL)
 	{
-		free(arr);
 		return (NULL);
 	}
 
-	for (f = 0; f < a; f++)
+	for (f = 0; f < l1; f++)
+	{
 		arr[f] = s1[f];
+	}
 
-	g = a;
-
-	for (a = 0; a <= g; f++, a++)
-		arr[f] = s2[a];
+	for (g = 0; g < l2; g++)
+	{
+		arr[f + g] = s2[g];
+	}
 	arr[f + g] = '\0';
-
 	return (arr);
 }
