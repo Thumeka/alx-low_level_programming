8685bad632b1fdbcc08d9b9f157e547bb470030e
Thumeka <thumekatembani21@gmail.com>
2023-05-18 13:52:33 -0700

    T1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -2,53 +2,54 @@
 #include <stdlib.h>
 
 /**
- * string_nconcat -  concatenates two strings.
- * @s1: first string
- * @s2: second string
+ * string_nconcat - concatenates two strings.
+ * @s1: first
+ * @s2: second
  * @n: bytes
  *
- * Return: NULL or point to memory.
+ * Return: NULL pointer to memory.
  */
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *arr;
-	unsigned int k = 0, p = 0, len1 = 0, len2 =0;
+	unsigned int len1 = 0, len2 = 0, k = 0, f = 0;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
 
-	while (s1[k])
-		k++;
-	while (s2[p])
-		p++;
+	while (s1[len1])
+		len1++;
 
-	if (n >= p)
-		len1 = k + p;
+	while (s2[len2])
+		len2++;
+
+	if (n >= len2)
+		f = len1 + len2;
 	else
-		len1 = k + n;
+		f = len1 + n;
 
-	arr = malloc(sizeof(char) * len1 + 1);
+	arr = malloc(sizeof(char) * f + 1);
 
 	if (arr == NULL)
 		return (NULL);
 
-	p = 0;
-	while (len2 < len1)
-	{
-		if (len2 <= k)
-			arr[len2] = s1[len2];
-		if (len2 >= k)
-		{
-			arr[len2] = s2[p];
-			p++;
-		}
+	len2 = 0;
 
-		len2++;
+	while (k < f)
+	{
+		if (k <= len1)
+			arr[k] = s1[k];
+		if (k >= len1)
+		{
+			arr[k] = s2[len2];
+			len2++;
+		}
 	}
 
-	arr[len2] = '\0';
+	arr[k] = '\0';
+
 	return (arr);
 }
