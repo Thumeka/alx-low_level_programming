452ce615b527f3d10056d07c85e47c2ef94235cc
Thumeka <thumekatembani21@gmail.com>
2023-05-22 15:58:21 -0700

    T1

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,16 +8,17 @@
  * @owner: char
  */
 
-struct dog
+typedef struct dog
 {
 	char *name;
 	char *owner;
 	float age;
-};
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+#include <stdlib.h>
 
 #endif /* DOG_H */
