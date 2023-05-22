fbd1998e551c63ecfea1c6e9694a389ec2b3bf13
Thumeka <thumekatembani21@gmail.com>
2023-05-22 14:48:41 -0700

    T0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Define a new type struct dog.
+ * @name: char
+ * @age: int
+ * @owner: char
+ */
+
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+#endif /* DOG_H */
