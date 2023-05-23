27b68c382fb0add79effd1f323591a9e82a1d408
Thumeka <thumekatembani21@gmail.com>
2023-05-22 17:14:44 -0700

    T2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,8 +13,8 @@ void print_dog(struct dog *d)
 {
 	if (d != NULL)
 	{
-		printf("Name: %s\n", (d->name != NULL) ? d->name : "(nil)");
+		printf("Name: %s\n", (d->name != NULL) ? d->name : "(nil)\n");
 		printf("Age: %.1f\n", d->age);
-		printf("Owner: %s\n", (d->owner != NULL) ? d->owner : "(nil)");
+		printf("Owner: %s\n", (d->owner != NULL) ? d->owner : "(nil)\n");
 	}
 }
