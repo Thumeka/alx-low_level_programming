70cd4b8f4ea86864a93a034c93b4d84256281205
Thumeka <thumekatembani21@gmail.com>
2023-06-01 06:30:18 -0700

    0-print_list.c

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+#include <stddef.h>
+#include <stdio.h>
+/**
+ * print_list - prints all the elements of a list_t list.
+ * @h: linked list
+ *
+ * Return: nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	int ls = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+
+		else
+		{
+			printf("[%u] %s\n", ls, h->str);
+		}
+
+		ls++;
+
+		h = h->next;
+	}
+
+	return (ls);
+}
