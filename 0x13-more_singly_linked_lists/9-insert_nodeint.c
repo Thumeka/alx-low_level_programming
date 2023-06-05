512a1e5629ebb3bdc1ab190a8df7234ed10807d8
Thumeka <thumekatembani21@gmail.com>
2023-06-05 14:04:14 -0700

    T9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -4,6 +4,27 @@
 #include "lists.h"
 
 /**
+ * new_node - creates new node
+ * @n: data
+ *
+ * Return: NULL or address
+ */
+
+listint_t *new_node(int n)
+{
+	listint_t *fresh_n;
+
+	if (fresh_n == NULL)
+	{
+		return (NULL);
+	}
+	fresh_n->n = n;
+	fresh_n->next = NULL;
+
+	return (NULL);
+}
+
+/**
  * insert_nodeint_at_index - inserts a new node at a given position.
  * @head: pointer
  * @idx: index
@@ -24,38 +45,31 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 	if (idx == 0)
 	{
-		fresh_n = malloc(sizeof(listint_t));
+		fresh_n = new_node(n);
 		if (fresh_n == NULL)
 		{
 			return (NULL);
 		}
-
-		fresh_n->n = n;
 		fresh_n->next = *head;
 		*head = fresh_n;
-
-		return (fresh_n);
+		return (NULL);
 	}
 
 	n_node = *head;
-	while (n_node != NULL)
+	while (n_node != NULL && j < idx - 1)
 	{
-		if (j == idx - 1)
-		{
-			fresh_n = malloc(sizeof(listint_t));
-			if (fresh_n == NULL)
-			{
-				return (NULL);
-			}
-		}
-
-		fresh_n->n = n;
-		fresh_n->next = n_node;
-		n_node->next = fresh_n;
-		return (fresh_n);
+		n_node = n_node->next;
+		j++;
 	}
-	j++;
-	n_node = n_node->next;
 
-	return (NULL);
+	fresh_n = new_node(n);
+	if (fresh_n == NULL)
+	{
+		return (NULL);
+	}
+
+	fresh_n->next = n_node->next;
+	n_node->next = fresh_n;
+
+	return (fresh_n);
 }
