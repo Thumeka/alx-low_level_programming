0b4017e10a3b6897a34322c64ee55c2217545dda
Thumeka <thumekatembani21@gmail.com>
2023-06-08 14:55:50 -0700

    T3

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -14,5 +14,7 @@ int set_bit(unsigned long int *n, unsigned int index)
 	if (index >= sizeof(unsigned long int) * 8)
 		return (-1);
 
-	return (*n |= 1UL << index);
+	*n |= 1UL << index;
+
+	return (1);
 }
