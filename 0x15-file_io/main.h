7eb342b600e471ad7522cc175ee401d44f8280d8
Thumeka <thumekatembani21@gmail.com>
2023-06-12 17:59:17 -0700

    T3

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -8,6 +8,8 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
+void __exit(int error, char *str, int open_f);
+int main(int argc, char *argv[]);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
