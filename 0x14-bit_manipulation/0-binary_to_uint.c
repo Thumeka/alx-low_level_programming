25c81bf3895fcfcb897ed52ac44df085fd23b7dd
root <you@example.com>
2023-07-06 07:49:36 -0700

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,17 +9,18 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-unsigned int bin = 0;
-int c = 0;
-if (b == NULL)
-return (0);
-while (b[c] != '\0')
-{
-if (b[c] != '0' && b[c] != '1')
-return (0);
-bin <<= 1;
-bin += b[c] - '0';
-c++;
-}
-return (bin);
+	unsigned int bin = 0;
+	int c = 0;
+
+	if (b == NULL)
+		return (0);
+	while (b[c] != '\0')
+	{
+		if (b[c] != '0' && b[c] != '1')
+			return (0);
+		bin <<= 1;
+		bin += b[c] - '0';
+		c++;
+	}
+	return (bin);
 }
