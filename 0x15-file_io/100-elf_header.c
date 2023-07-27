036db9b816d16d23c4bcf2f3d31df2e4fd18b962
root <you@example.com>
2023-07-27 02:56:24 -0700

    100-elf_header.c

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,7 +1,3 @@
-/*
-* File: 100-elf_header.c
-* Auth: Brennan D Baraban
-*/
 #include <elf.h>
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -9,6 +5,7 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+
 void check_elf(unsigned char *e_ident);
 void print_magic(unsigned char *e_ident);
 void print_class(unsigned char *e_ident);
