f5d3c80dea515224b813c022117f7b5e36a5724d
root <you@example.com>
2023-07-27 01:45:09 -0700

    retry

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -5,255 +5,201 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <elf.h>
-
-int check_elf(char *e);
-void print_magic(char *e);
-void print_class(char *e);
-void print_data(char *e);
-void print_version(char *e);
-void print_osabi(char *e);
-void print_type(char *e);
-void print_entry(char *e);
-
 /**
-* print_magic - Prints the magic numbers
-* @e: A pointer
-*
-* Description: separated by spaces.
+* addr - prints address
+* @ptr: magic.
+* Return: no return.
 */
-
-void print_magic(char *e)
+void addr(char *ptr)
 {
-	int index;
-
-	printf("  Magic:  ");
-
-	for (index = 0; index < 16; index++)
-		printf(" %02x", e[index]);
-	printf("\n");
+int i;
+int begin;
+char sys;
+printf(" Entry point address: 0x");
+sys = ptr[4] + '0';
+if (sys == '1')
+{
+begin = 26;
+printf("80");
+for (i = begin; i >= 22; i--)
+{
+if (ptr[i] > 0)
+printf("%x", ptr[i]);
+else if (ptr[i] < 0)
+printf("%x", 256 + ptr[i]);
 }
-
-/**
-* print_data - Prints the data
-* @e: A pointer
-*/
-
-void print_data(char *e)
-{
-	char print_data = e[5];
-
-	printf("  Data:                              2's complement");
-	if (print_data == 1)
-		printf(", little endian\n");
-	else if (print_data == 2)
-		printf(", big endian\n");
-	else
-		printf("\n");
+if (ptr[7] == 6)
+printf("00");
 }
-
-/**
-* print_version - Prints the version
-* @e: A pointer
-*/
-
-void print_version(char *e)
+if (sys == '2')
 {
-	int print_version = e[6];
-
-	printf(" Version:                           %d", print_version);
-
-	if (print_version == EV_CURRENT)
-		printf(" (current)");
-	printf("\n");
-
+begin = 26;
+for (i = begin; i > 23; i--)
+{
+if (ptr[i] >= 0)
+printf("%02x", ptr[i]);
+else if (ptr[i] < 0)
+printf("%02x", 256 + ptr[i]);
 }
-
-/**
-* print_osabi - Prints the OS/ABI
-* @e: A pointer
-*/
-
-void print_osabi(char *e)
-{
-	char print_osabi = e[7];
-
-	printf("  OS/ABI:                            ");
-	if (print_osabi == 0)
-		printf("UNIX - System V\n");
-	else if (print_osabi == 2)
-		printf("UNIX - NetBSD\n");
-	else if (print_osabi == 6)
-		printf("UNIX - Solaris\n");
-	else
-		printf("<unknown; %x>\n", print_osabi);
-
-	printf("  ABI Version:                       %d\n", e[8]);
-
+}
+printf("\n");
 }
 /**
-* print_type - Prints the type
-* @e: The ELF type.
+* type - prints type
+* @ptr: magic.
+* Return: no return.
 */
-
-void print_type(char *e)
+void type(char *ptr)
 {
-	char print_type;
-
-	if (e[5] == 1)
-		print_type = e[16];
-	else
-		print_type = e[17];
-	printf("  Type:                              ");
-	if (print_type == 0)
-		printf("NONE (No file type)\n");
-	else if (print_type == 1)
-		printf("REL (Relocatable file)\n");
-	else if (print_type == 2)
-		printf("EXEC (Executable file)\n");
-	else if (print_type == 3)
-		printf("DYN (Shared object file)\n");
-	else if (print_type == 4)
-		printf("CORE (Core file)\n");
-	else
-		printf("<unknown: %x>\n", print_type);
+char type = ptr[16];
+if (ptr[5] == 1)
+type = ptr[16];
+else
+type = ptr[17];
+printf(" Type: ");
+if (type == 0)
+printf("NONE (No file type)\n");
+else if (type == 1)
+printf("REL (Relocatable file)\n");
+else if (type == 2)
+printf("EXEC (Executable file)\n");
+else if (type == 3)
+printf("DYN (Shared object file)\n");
+else if (type == 4)
+printf("CORE (Core file)\n");
+else
+printf("<unknown: %x>\n", type);
 }
-
 /**
-* print_entry - Prints the entry point
-* @e: The address
+* osabi - prints osabi
+* @ptr: magic.
+* Return: no return.
 */
-
-void print_entry(char *e)
+void osabi(char *ptr)
 {
-	int j;
-	int start;
-	char sys;
-
-	printf("  Entry point address:               0x");
-
-	sys = e[4];
-	if (sys == 1)
-	{
-		start = 26;
-		printf("80");
-		for (j = start; j >= 22; j--)
-		{
-			if (e[j] > 0)
-				printf("%x", e[j]);
-			else if (e[j] < 0)
-				printf("%x", 256 + e[j]);
-		}
-		if (e[7] == 6)
-			printf("00");
-	}
-	if (sys == 2)
-	{
-		start = 26;
-		for (j = start; j > 23; j--)
-		{
-			if (e[j] >= 0)
-				printf("%02x", e[j]);
-			else if (e[j] < 0)
-				printf("%02x", 256 + e[j]);
-		}
-	}
-	printf("\n");
+char osabi = ptr[7];
+printf(" OS/ABI: ");
+if (osabi == 0)
+printf("UNIX - System V\n");
+else if (osabi == 2)
+printf("UNIX - NetBSD\n");
+else if (osabi == 6)
+printf("UNIX - Solaris\n");
+else
+printf("<unknown: %x>\n", osabi);
+printf(" ABI Version: %d\n", ptr[8]);
 }
-
 /**
-* print_class - Prints the class of an ELF header.
-* @e: A pointer to an array containing the ELF class.
+* version - prints version
+* @ptr: magic.
+* Return: no return.
 */
-void print_class(char *e)
+void version(char *ptr)
 {
-	char print_class = e[4];
-
-	if (print_class == 0)
-	{
-		fprintf(stderr, "Error: Not an ELF file\n");
-		exit(98);
-	}
-
-	printf("ELF Header:\n");
-	print_magic(e);
-
-	if (print_class == 1)
-		printf("  Class:                             ELF32\n");
-
-	else if (print_class == 2)
-		printf("  Class:                             ELF64\n");
-	else
-	{
-		fprintf(stderr, "Error: Not an ELF file\n");
-		exit(98);
-	}
-
-	print_data(e);
-	print_version(e);
-	print_osabi(e);
-	print_type(e);
-	print_entry(e);
+int version = ptr[6];
+printf(" Version: %d", version);
+if (version == EV_CURRENT)
+printf(" (current)");
+printf("\n");
 }
-
 /**
-* check_elf - Checks if elf file
-* @e: pointer
-*
-* Return: 1 or 0
+* data - prints data
+* @ptr: magic.
+* Return: no return.
 */
-int check_elf(char *e)
+void data(char *ptr)
 {
-	int print_entry = (int)e[0];
-	char E = e[1];
-	char L = e[2];
-	char F = e[3];
-
-	if (print_entry == 127 && E == 'E' && L == 'L' && F == 'F')
-		return (1);
-	return (0);
+char data = ptr[5];
+printf(" Data: 2's complement");
+if (data == 1)
+printf(", little endian\n");
+if (data == 2)
+printf(", big endian\n");
 }
-
 /**
-* main - Displays the information contained
-* ELF header
-* @argc: count
-* @argv: vector
-*
-* Return: 0
-*
-* Description: If the file is not an ELF File or exit code 98.
+* magic - prints magic info.
+* @ptr: magic.
+* Return: no return.
 */
-
+void magic(char *ptr)
+{
+int bytes;
+printf(" Magic: ");
+for (bytes = 0; bytes < 16; bytes++)
+printf(" %02x", ptr[bytes]);
+printf("\n");
+}
+/**
+* check_sys - check the version system.
+* @ptr: magic.
+* Return: no return.
+*/
+void check_sys(char *ptr)
+{
+char sys = ptr[4] + '0';
+if (sys == '0')
+exit(98);
+printf("ELF Header:\n");
+magic(ptr);
+if (sys == '1')
+printf(" Class: ELF32\n");
+if (sys == '2')
+printf(" Class: ELF64\n");
+data(ptr);
+version(ptr);
+osabi(ptr);
+type(ptr);
+addr(ptr);
+}
+/**
+* check_elf - check if it is an elf file.
+* @ptr: magic.
+* Return: 1 if it is an elf file. 0 if not.
+*/
+int check_elf(char *ptr)
+{
+int addr = (int)ptr[0];
+char E = ptr[1];
+char L = ptr[2];
+char F = ptr[3];
+if (addr == 127 && E == 'E' && L == 'L' && F == 'F')
+return (1);
+return (0);
+}
+/**
+* main - check the code for Holberton School students.
+* @argc: number of arguments.
+* @argv: arguments vector.
+* Return: Always 0.
+*/
 int main(int argc, char *argv[])
 {
-	int fd, r_read;
-	char p[27];
-
-	if (argc != 2)
-	{
-		dprintf(STDERR_FILENO, "Usage: elf_header elf_filename\n");
-		exit(98);
-	}
-	fd = open(argv[1], O_RDONLY);
-	if (fd < 0)
-	{
-		dprintf(STDERR_FILENO, "Err: file can not be open\n");
-		exit(98);
-	}
-	lseek(fd, 0, SEEK_SET);
-	r_read = read(fd, p, 27);
-
-	if (r_read == -1)
-	{
-		dprintf(STDERR_FILENO, "Err: The file can not be read\n");
-		exit(98);
-	}
-	if (!check_elf(p))
-	{
-		dprintf(STDERR_FILENO, "Err: It is not an ELF\n");
-		exit(98);
-	}
-	print_class(p);
-	close(fd);
-	return (0);
+int fd, ret_read;
+char ptr[27];
+if (argc != 2)
+{
+dprintf(STDERR_FILENO, "Usage: elf_header elf_filename\n");
+exit(98);
+}
+fd = open(argv[1], O_RDONLY);
+if (fd < 0)
+{
+dprintf(STDERR_FILENO, "Err: file can not be open\n");
+exit(98);
+}
+lseek(fd, 0, SEEK_SET);
+ret_read = read(fd, ptr, 27);
+if (ret_read == -1)
+{
+dprintf(STDERR_FILENO, "Err: The file can not be read\n");
+exit(98);
+}
+if (!check_elf(ptr))
+{
+dprintf(STDERR_FILENO, "Err: It is not an ELF\n");
+exit(98);
+}
+check_sys(ptr);
+close(fd);
+return (0);
 }
