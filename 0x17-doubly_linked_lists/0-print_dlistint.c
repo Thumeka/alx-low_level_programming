0562bbcdd6847c425ba1b0f61b43e04c0cda8601
thumeka <thumekatembani21@gmail.com>
2023-08-17 08:09:18 -0700

    0-print_dlistint.c

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - print the listint
+ * @h: head pointer
+ *
+ * Return: number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t elem = 0;
+	const dlistint_t *current = h;
+
+	while (h != NULL)
+	{
+		printf("%d\n", current->n);
+		current = current->next;
+		elem++;
+	}
+	return (elem);
+}
