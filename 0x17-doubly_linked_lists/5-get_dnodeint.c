3651f9e7e9c0e0fd15a50d2246bac09edb98b61e
thumeka <thumekatembani21@gmail.com>
2023-08-17 09:29:22 -0700

    5

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - node at the index
+ * @index: index
+ * @head: pointer
+ * Return: node
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	if (!head)
+		return (NULL);
+	while (index && head->next)
+	{
+		head = head->next;
+		index -= 1;
+	}
+
+	if (index == 0)
+		return (head);
+	return (NULL);
+}
