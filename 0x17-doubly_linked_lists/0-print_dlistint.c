87aa7b1b5f321c494bc2521de0edca1fda96e098
thumeka <thumekatembani21@gmail.com>
2023-08-17 08:17:13 -0700

    0-print_dlistint.c

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -11,7 +11,7 @@ size_t print_dlistint(const dlistint_t *h)
 	size_t elem = 0;
 	const dlistint_t *current = h;
 
-	while (h != NULL)
+	while (current != NULL)
 	{
 		printf("%d\n", current->n);
 		current = current->next;
