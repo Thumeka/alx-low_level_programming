f6852ad3a7d0cf7b486ca3cc375caeedff444819
thumeka <thumekatembani21@gmail.com>
2023-09-08 05:36:27 -0700

    bets

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -5,10 +5,49 @@
  * @ht: to add
  * @key: the key
  * @value: empty string
- * Return 1 or 0
+ * Return: 1 or 0
  *
  **/
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	;;
+	unsigned long int index;
+	hash_node_t *node, *current;
+
+	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
+		return (0);
+	index = key_index((const unsigned char *)key, ht->size);
+	if (ht->array[index])
+	{
+		current = ht->array[index];
+		while (current)
+		{
+			if (strcmp(current->key, (char *)key) == 0)
+			{
+				free(current->value);
+				current->value = (char *) strdup(value);
+				return (1);
+			}
+			current = current->next;
+		}
+	}
+	node = malloc(sizeof(hash_node_t));
+	if (node == NULL)
+		return (0);
+	node->key = (char *)strdup(key);
+	if (node->key == NULL)
+	{
+		free(node);
+		return (0);
+	}
+	node->value = (char *)strdup(value);
+	if (node->value == NULL)
+	{
+		free(node->key);
+		free(node);
+		return (0);
+	}
+	node->next = ht->array[index];
+	ht->array[index] = node;
+	return (1);
+}
