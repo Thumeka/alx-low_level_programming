f2fbb499038a62632dec2435b7ce15ad1f8a3587
thumeka <thumekatembani21@gmail.com>
2023-09-08 05:50:12 -0700

    indent

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -20,9 +20,9 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 			node = ht->array[index];
 			while (node)
 			{
-				if (strcmp(node->key, (char *)key) == 0)
-						return (node->value);
-					node = node->next;
+				if (strcmp(node->key, key) == 0)
+					return (node->value);
+				node = node->next;
 			}
 		}
 	}
